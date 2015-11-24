312b2914629b4bdc2196a56c580d194d43147e5f
easonlin win7 <easonlin404@gmail.com>
2015-11-24 11:20:12 +0800

    create

---
+++ src/main/java/masterSpringMVC/controller/HelloController.java
@@ -0,0 +1,15 @@
+package masterSpringMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from the controller");
+        return "resultPage";
+    }
+}
